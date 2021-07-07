b2527cf209c1851d1420ac7e33f9222d4e8304e0
miahljnh97 <mia.huljanah@sci.ui.ac.id>
2021-07-07 22:29:15 +0700

    first commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+console.log("JavaScript success");
+
+//edited profile
+document.getElementById('name').innerHTML = 'Mia Huljanah';
+document.getElementById('date').innerHTML = '1997';
+document.getElementById('age').innerHTML = '24 y.o';
+document.getElementById('city').innerHTML = 'Jakarta';
